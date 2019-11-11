b0df2002b93a64cccbc59fdaa44702da0564a77a
Claudiu Gombos <cgombos@smallfootprint.com>
2019-11-11 21:30:51 +0200

    counter example

--- src/Counter.js
+++ src/Counter.js
@@ -1,8 +1,42 @@
 import React from 'react';
 
 class Counter extends React.Component {
+
+    constructor() {
+      super();
+      this.state = {
+        counter: 0
+      };
+      this.dec = this.dec.bind(this);
+    }
+    inc = () => {
+      debugger;
+      this.setState(
+        {
+          counter: this.state.counter +1
+        }
+      );
+    }
+
+    dec() {
+      this.setState(
+        {
+          counter: this.state.counter -1
+        }
+      );
+    }
+    
     render() {
-      return <h2>Hi, I am a Car!</h2>;
+      return (
+      <div>
+        <button 
+        onClick={this.dec}
+        >-</button>
+
+        <input type="text" value={this.state.counter} ></input>
+
+        <button  onClick={this.inc}>+</button>
+      </div>);
     }
   }
 
